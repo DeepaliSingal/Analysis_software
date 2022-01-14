c6e9d0f57a58dc0bb6cc3a41a14c35a1e95058d2
“deepali2002-max” <deepalisingal2002@gmail.com>
2022-01-14 23:44:03 +0530

    added new files

--- app/src/main/java/com/example/analysis/MainActivity.java
+++ app/src/main/java/com/example/analysis/MainActivity.java
@@ -5,17 +5,67 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView sample;
+    EditText username,password,confirmpassword,email;
+    TextView register,existingusers;
+    DBHelper DB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sample=(TextView)findViewById(R.id.first);
-        sample.setOnClickListener(new View.OnClickListener() {
+        username=(EditText) findViewById(R.id.PersonName);
+        password=(EditText) findViewById(R.id.Password);
+        confirmpassword=(EditText) findViewById(R.id.confirmPassword);
+        email=(EditText) findViewById(R.id.EmailAddress);
+        register=(TextView) findViewById(R.id.signup);
+        existingusers=(TextView) findViewById(R.id.loginhere);
+        DB= new DBHelper(this);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user=username.getText().toString();
+                String pass=password.getText().toString();
+                String repass=confirmpassword.getText().toString();
+                String em=email.getText().toString();
+
+                if(user.equals("")||pass.equals("")||repass.equals("")||em.equals(""))
+                    Toast.makeText(MainActivity.this, "PLEASE FILL ALL THE FIELDS", Toast.LENGTH_SHORT).show();
+                else
+                {
+                    if(pass.equals(repass)){
+                        Boolean checkuser=DB.checkusername(user);
+                        if(checkuser==false){
+                            Boolean insert=DB.insertData(user,pass,em);
+                            if(insert==true){
+                                Toast.makeText(MainActivity.this, "REGISTERED SUCCESSFULLY", Toast.LENGTH_SHORT).show();
+
+                            }
+                            else
+                            {
+                                Toast.makeText(MainActivity.this, "REGISTRATION FAILED", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else
+                        {
+                            Toast.makeText(MainActivity.this, "USER ALREADY EXISTS!! PLEASE LOGIN", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else
+                    {
+                        Toast.makeText(MainActivity.this, "PASSWORDS DON'T MATCH", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        existingusers.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent= new Intent(getApplicationContext(),login.class);
--- app/src/main/java/com/example/analysis/login.java
+++ app/src/main/java/com/example/analysis/login.java
@@ -1,15 +1,73 @@
 package com.example.analysis;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
+
+    EditText email,password;
+    TextView enter, signinhere, forgotpassword;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login2);
-
+        setContentView(R.layout.activity_login);
+        email=(EditText) findViewById(R.id.EmailAddress1);
+        password=(EditText) findViewById(R.id.Password1);
+        enter=(TextView) findViewById(R.id.login);
+        forgotpassword=(TextView) findViewById(R.id.forgotpass);
+        signinhere=(TextView) findViewById(R.id.signinhere);
+        DB=new DBHelper(this);
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user=email.getText().toString();
+                String pass=password.getText().toString();
+                if(user.equals("")||pass.equals("")){
+                    Toast.makeText(login.this, "PLEASE FILL ALL THE FIELDS", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Boolean checkuserpass=DB.checkusernamepassword(user,pass);
+                    if(checkuserpass==true)
+                    {
+                        Toast.makeText(login.this, "LOGGED IN SUCCESSFULLY", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(login.this, "INVALID CREDENTIALS", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        forgotpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user=email.getText().toString();
+                Boolean newcred=DB.deletedata(user);
+                if(newcred==true)
+                {
+                    Toast.makeText(login.this,"NOW YOU CAN SIGNUP WITH NEW CREDENTIALS",Toast.LENGTH_SHORT).show();
+                    Intent intent= new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                }
+                else
+                    Toast.makeText(login.this,"PLEASE TRY AGAIN LATER!!",Toast.LENGTH_SHORT).show();
+            }
+        });
+        signinhere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
